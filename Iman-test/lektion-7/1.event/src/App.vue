1da29a3ae8932ab84857ecddd7484a9ab1a53a43
Iman.ezziyadi <iman.ezziyadi@utb.ecutbildning.se>
2021-02-24 13:26:51 +0100

    update

---
+++ Iman-test/lektion-7/1.event/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+  <hero />
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-6">
+       <sign-in />
+      </div>
+       <div class="col-6">
+       <register />
+       </div>
+  </div>
+  </div>
+  
+</div>
+</template>
+
+<script>
+import Hero from './components/Hero'
+import signIn from './components/signIn'
+ import Register from './components/Register'
+export default {
+  name: 'App',
+  components: {
+    Hero,
+    signIn,
+    Register
+  }
+}
+</script>
+
+<style>
+
+</style>
