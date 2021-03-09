97633eec82206562790368d1230ddd6b845831e1
Iman.ezziyadi <iman.ezziyadi@utb.ecutbildning.se>
2021-03-09 19:25:45 +0100

    update

---
+++ Iman-lektion/lektion9/1.todo/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="bg-light">
+    <Header />
+
+      </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+   
+  }
+}
+</script>
+
+<style>
+
+
+</style>
---
+++ Iman-lektion/lektion9/1.todo/src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="navbar navbare-dark bg-info">
+<h1 class="navbar-brand">TODO<h1>
+</nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
